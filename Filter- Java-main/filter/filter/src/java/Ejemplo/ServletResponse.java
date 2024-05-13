803b5c0d4f55297ebb0823ee288bbbe706106d4f
osvaldo16lopezromo <127708644+osvaldo16lopezromo@users.noreply.github.com>
2024-05-12 20:33:00 -0600

    Add files via upload

---
+++ Filter- Java-main/filter/filter/src/java/Ejemplo/ServletResponse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejemplo;
+
+import java.io.OutputStream;
+
+/**
+ *
+ * @author sch
+ */
+class ServletResponse {
+
+    void setContentType(String texthtml) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    OutputStream getOutputStream() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
